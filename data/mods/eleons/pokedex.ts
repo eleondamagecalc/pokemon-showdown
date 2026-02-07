4660a951fd51be5aca20fabcb24f685327df5fa1
eleondamagecalc <neoblackwood8@gmail.com>
2026-02-07 00:04:36 -0500

    Add files via upload

--- data/mods/eleons/pokedex.ts
+++ data/mods/eleons/pokedex.ts
@@ -1,7 +1,7 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-  eleonone: {
+  bombossic: {
     num: -79,
-    name: "Eleonone",
+    name: "Bombossic",
     types: ["Electric"],
     baseStats: {hp: 75, atk: 110, def: 70, spa: 95, spd: 70, spe: 120},
     abilities: {0: "Blaze", H: "Lightning Rod"},
